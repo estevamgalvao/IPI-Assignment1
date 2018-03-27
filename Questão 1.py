9e0308cf04e92d7f4510f9509eae5946b9c3b849
Estevam Galvão <33361102+estevamgalvao@users.noreply.github.com>
2018-03-27 18:33:37 -0300

    add while, imwrites, fix
    resolvido while infinito
    o código agora cria as imagens: "Mars_grey", "Mars_greyWay", "Mars_Way". As duas últimas mostram o caminho
    fixado problemas de cálculos

--- Questão 1.py
+++ Questão 1.py
@@ -1,51 +1,53 @@
 import numpy as np
 import cv2
+import copy
 
 from rgb2gray import rgb2grey
 from calc_distancia import calc_distancia
 
 imgMars = cv2.imread("Mars.bmp")
+imgMars_aux = copy.copy(imgMars)
 height, width, channels = imgMars.shape
 
-
 #imgRGB 3 canais -> imgGrey ainda com 3 canais.
-imgMars_grey = rgb2grey(height, width, imgMars)
+imgMars_grey = rgb2grey(height, width, imgMars_aux)
 
-#Isso prova que estou preenchendo as 3 matrizes R,G,B com o mesmo valor.
-# print(img_mars[i,j,0])
-# print(img_mars[i,j,1])
-# print(img_mars[i,j,2])
+cv2.imshow("Mars.bmp", imgMars_aux)
+cv2.imwrite("Mars_grey.bmp", imgMars_grey)
+cv2.waitKey(0)
 
 #fazer função que realize a equalização da img
 
 pAtual = [415, 260]
 pDestino = [1000, 815]
 
-lista_distancias = calc_distancia(pAtual[0],pAtual[1],pDestino[0],pDestino[1])
-lista_distancias.sort()
+loucura = 10000
+while(pAtual[0] != pDestino[0] and pAtual[1] != pDestino[1]):
+    lista_distancias = calc_distancia(pAtual[0],pAtual[1],pDestino[0],pDestino[1])
 
-print(lista_distancias)
+    lista_distancias.sort()
+    candidatos = [lista_distancias[0],lista_distancias[1], lista_distancias[2]]
 
-#o sort dentro do while tá dando q compara complex com float
+    #candidatos [[181, 838, 734], [180, 839, 734], [179, 840, 734]
+    #lista_distancia [[181, 838, 734], [180, 839, 734], [179, 840, 734], [181, 838, 733], [179, 840, 733], [182, 838, 732], [181, 839, 732], [180, 840, 732]]
 
-# while(pAtual != pDestino):
-#     lista_distancias = calc_distancia(pAtual[0],pAtual[1],pDestino[0],pDestino[1])
-#     lista_distancias.sort()
-#     candidatos = [lista_distancias[0],lista_distancias[1], lista_distancias[2]]
-#
-#     print(candidatos[0][1])
-#     print(candidatos[0][2])
-#     print(imgMars_grey[416,261])
-#
-#     aux = 9999
-#     for i in range(3):#[0,3)
-#         if(imgMars_grey[candidatos[i][1],candidatos[i][2],0] <= aux): #talvez o <= cause um caminho diferente
-#             aux = imgMars_grey[candidatos[i][1],candidatos[i][2],0]
-#             pAtual[0] = candidatos[i][1]
-#             pAtual[1] = candidatos[i][2]
-#
-#     print(pAtual[0])
-#     imgMars_grey[pAtual[0],pAtual[1],2] = 255
-#
-# cv2.imshow("Mars.bmp", imgMars_grey)
-# cv2.waitKey(0)
+    aux = 9999
+    for i in range(3):#[0,3)
+        if(imgMars_grey[candidatos[i][2],candidatos[i][1],0] <= aux): #talvez o <= cause um caminho diferente
+            aux = imgMars_grey[candidatos[i][2],candidatos[i][1],0]
+            pAtual[1] = candidatos[i][2]
+            pAtual[0] = candidatos[i][1]
+
+    imgMars_grey[pAtual[1],pAtual[0],2] = 255
+    imgMars_grey[pAtual[1],pAtual[0],1] = 0
+    imgMars_grey[pAtual[1],pAtual[0],0] = 0
+
+    imgMars[pAtual[1],pAtual[0],2] = 0
+    imgMars[pAtual[1],pAtual[0],1] = 0
+    imgMars[pAtual[1],pAtual[0],0] = 0
+
+cv2.imshow("Mars_grey.bmp", imgMars_grey)
+cv2.waitKey(0)
+
+cv2.imwrite("Mars_greyWay.bmp", imgMars_grey)
+cv2.imwrite("Mars_Way.bmp", imgMars)
