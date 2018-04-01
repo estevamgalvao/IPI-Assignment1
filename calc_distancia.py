721ee356b539b5e66a0dbfe51aa8ef81918df7f5
Estevam Galvão <33361102+estevamgalvao@users.noreply.github.com>
2018-04-01 18:56:42 -0300

    Update calc_distancia.py
    adicionados comentários

--- calc_distancia.py
+++ calc_distancia.py
@@ -1,11 +1,9 @@
 #[Sudoeste, Sul, Sudeste] =  [(x-1;y+1), (x;y+1), (x+1;y+1)]  =  [0,1,2]
 #[Oeste ,  Centro  , Leste]  =  [(x-1;y)  , (x;y)  , (x+1;y)  ]  =  [3,x,4]
 #[Noroeste , Norte , Nordeste]  =  [(x-1;y-1), (x;y-1), (x+1;y-1)]  =  [5,6,7]
-#
-#d = sqrt((bx-ax)^2 + (by+ay)^2)
-#
-import math
 
+# Atribuo nomes de direções para facilitar no entendimento
+# Atribuo os nomes invertidos pois as coordenadas da matriz da imagem crescem de cima pra baixo
 
 def calc_distancia(atualX, atualY, destinoX, destinoY):
   listDistancias = [0,0,0,0,0,0,0,0]
@@ -43,5 +41,4 @@ def calc_distancia(atualX, atualY, destinoX, destinoY):
   listDistancias[6] = [norte, atualX, atualY-1]
   listDistancias[7] = [nordeste, atualX+1, atualY-1]
 
-  #print(listDistancias)
   return listDistancias
